6a0cd11f7f337e6d0cf23b1e96107d6369056283
Victour Bue <victourmbue@gmail.com>
2021-10-29 11:06:22 -0600

    initial commit, currently just displays test level

---
+++ code/main.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from settings import *
+from tiles import Tile
+from level import Level
+
+#General setup
+pygame.init()
+clock = pygame.time.Clock()
+
+# Setting up main window
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption(game_title)
+pygame.display.set_icon(pygame.image.load(game_icon))
+
+# instatiate objects
+level = Level(level_map,screen)
+
+
+
+
+
+while True:
+    # Input Handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    # Updating the Window
+    screen.fill((55,50,60))
+    level.run()
+    pygame.display.flip()
+    clock.tick(60)
---
+++ code/settings.py
@@ -0,0 +1,18 @@
+level_map = [
+    '                      X       ',
+    '                          XX  ',
+    'XXXX        XXXX              ',
+    'XXX        XXXXX              ',
+    'X                          XX ',
+    '       X           XXX   XXXX ',
+    '  XX  XXX   XX XX  XXX    XXX ',
+    ' XXX  XXXX  XX XX  XXXX    XX ',
+    'XXXX  XXXX  XXXXX  XXXX    XX ',
+    'XXXX  XXXX  XXXXX  XXXXX   XX ']
+
+tile_size = 64
+screen_width = 1280
+screen_height = len(level_map) * tile_size    
+
+game_title = "Pitts the Platforming Pirate"
+game_icon = "graphics/character/hat.png"
